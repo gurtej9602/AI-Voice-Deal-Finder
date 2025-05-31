133a8485de1c34a5bb26dbef40163d8432331f7d
Gurtej Singh <129028020+gurtej9602@users.noreply.github.com>
2025-05-31 18:49:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+import os
 from flask import Flask, request, jsonify, render_template
 import requests
 from flask_cors import CORS
@@ -70,4 +70,5 @@ def search():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
